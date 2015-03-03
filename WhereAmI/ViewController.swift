afe2ab40ffc6b198b39eefa9357a26d439147dcc
Rommel Rico <rommeltj@gmail.com>
2015-03-02 21:34:23 -0800

    Building the app.

--- WhereAmI/ViewController.swift
+++ WhereAmI/ViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var manager:CLLocationManager!
+    
+    @IBOutlet weak var myLatitudeLabel: UILabel!
+    @IBOutlet weak var myLongitudeLabel: UILabel!
+    @IBOutlet weak var mySpeedLabel: UILabel!
+    @IBOutlet weak var myHeadingLabel: UILabel!
+    @IBOutlet weak var myAltitudeLabel: UILabel!
+    @IBOutlet weak var myNearestAddressTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userLocation:CLLocation = locations[0] as CLLocation
+        
+        //Latitude
+        myLatitudeLabel.text = "Latitude: \(userLocation.coordinate.latitude)"
+        
+        //Longitude
+        myLongitudeLabel.text = "Longitude: \(userLocation.coordinate.longitude)"
+        
+        //Speed
+        mySpeedLabel.text = "Speed: \(userLocation.speed)"
+        
+        //Heading
+        myHeadingLabel.text = "Heading: \(userLocation.course)"
+        
+        //Altitude 
+        myAltitudeLabel.text = "Altitude: \(userLocation.altitude)"
+        
+        //Nearest Address
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: { (placemarks, error) -> Void in
+            if (error != nil) {
+                NSLog("ERROR: \(error)")
+            } else {
+                let place = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                
+                var subThoroughfare = ""
+                if (place.subThoroughfare != nil) {
+                    subThoroughfare = place.subThoroughfare
+                }
+                
+                self.myNearestAddressTextView.text = "\(subThoroughfare) \(place.thoroughfare) \n \(place.subLocality) \n \(place.subAdministrativeArea) \n \(place.country) \(place.postalCode) "
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
